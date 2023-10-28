3d417d28a1e0e5b449adce40032864b12bb877ac
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2023-10-28 20:14:52 -0300

    finalizei o ex 15

--- ex-aula12/ex-015/script.js
+++ ex-aula12/ex-015/script.js
@@ -1,1 +1,28 @@
-alert('Olá!')
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+
+msg.innerHTML = `Agora são ${hora} horas!`
+
+if (hora >= 0 && hora < 12) {
+    // Bom dia
+    img.src = 'fotomanha.png'
+    document.body.style.background = '#eac6a6'
+    document.getElementById('msg').style.color = '#eac6a6'
+} else if (hora >= 12 && hora <= 18) {
+    //Boa tarde
+    img.src = 'fototarde.png'
+    document.body.style.background = '#77524a'
+    document.getElementById('msg').style.color = '#77524a'
+} else {
+    //Boa noite
+    img.src = 'fotonoite.png'
+    document.body.style.background = '#03264f'
+    document.getElementById('msg').style.color = '#03264f'
+
+}
+}
+
