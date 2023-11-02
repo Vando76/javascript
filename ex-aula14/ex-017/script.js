455fb84f5ac86b902dfe3bb9ed2265813870f310
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2023-11-02 20:55:32 -0300

    Iniciei o ex 17

---
+++ ex-aula14/ex-017/script.js
@@ -0,0 +1,14 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Seus dados estão incompletos!')
+    } else {
+        res.innerHTML = `Contando...`
+    }
+    
+    
+} 
