9e8b21a40f1664b9a6eb9622f6e33b66295e552c
Vandeilson Soares <133666770+Vando76@users.noreply.github.com>
2023-11-03 20:09:04 -0300

    Atualizei o EX-17

--- ex-aula14/ex-017/script.js
+++ ex-aula14/ex-017/script.js
@@ -5,10 +5,35 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Seus dados estão incompletos!')
+        //window.alert('[ERRO] Seus dados estão incompletos!')
+        res.innerHTML = 'Impossível contar'
     } else {
-        res.innerHTML = `Contando...`
+        res.innerHTML = `Contando: <br>`
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            
+        
+           } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+           }
+           res.innerHTML += `\u{1F3C1}`
+
+           
+        
     }
     
     
-} 
+}
